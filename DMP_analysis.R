e565196353b744576c45adfecfdf1e3b18f5eb8a
MinhphamMizzou <48994298+MinhphamMizzou@users.noreply.github.com>
2021-12-18 16:06:41 -0600

    Add files via upload

--- DMP_analysis.R
+++ DMP_analysis.R
@@ -1,168 +1,151 @@
 #---retrieve packages 
 library(readxl)
-library(tidyr)
 library(ggplot2)
 library(dplyr)
-library(psych)
 library(scales)
 library(tidytext)
+library(jtools)
+
+
+
 
 
 #---import data
 dtm<- read_excel("/Users/minhpham/Downloads/DMP_Scoring.xlsx", 1)
-View(dtm)
+dtm <- dtm[, -1]
 
-#---process data for the analysis
 str(dtm)
 dtm$O5 <- as.numeric(dtm$O5)
 dtm$O6 <- as.numeric(dtm$O6)
 dtm$O8 <- as.numeric(dtm$O8)
 
-dtm_1 <- dplyr::select(dtm, O1:A8)
+#----composite score distribution for plans
+dtm_cs <- dtm[, -1]
+#-transpose
+dtm_cs_t <- t(dtm_cs)
+dtm_cs_t <- as.data.frame(dtm_cs_t)
 
-dtm$criteria_s <- dtm_1%>%
+dtm_cs_t$score <- dtm_cs_t%>%
   summarise(mean = round(rowMeans(., na.rm = T), 2)) # criteria score
 
-str(dtm)
+score_d <- dtm_cs_t%>%
+  group_by(score)%>%
+  summarize(count=n())
+
+score_d <- as.data.frame(score_d)
+
+score_d$score <- ifelse(score_d$score<1, "<1",
+                        ifelse(score_d$score>=1 & score_d$score<=1.25, "1-1.25",
+                               ifelse(score_d$score>1.25 & score_d$score<=1.50, "1.26-1.50",
+                                      ">1.50")))
+
+t <- score_d%>%
+  group_by(score)%>%
+  summarise(count = sum(count))%>%
+  arrange(desc(count))
+  
+t$score <- unlist(t$score)
+
+t <- as.data.frame(t)
+
+
+t$percent <- round((t$count/21)*100, 2)
+
+
+t$score <- factor(t$score, levels = c("<1", "1-1.25", "1.26-1.50", ">1.50"))
+
+
+plot1 <- t%>%
+    ggplot(aes(x = score, 
+               y = (t$count/21))) +
+    geom_bar(stat = "identity", alpha = .5) +
+    geom_text(aes(label = percent(count/21), vjust = -0.2)) +
+    labs (
+      x = "Composite Score", y = NULL)+
+  scale_y_continuous(labels = scales::percent)
+
+
+plot1 + theme_apa()
+  
+# Scores of individual scorecards
+dtm_1 <- dplyr::select(dtm, O1:A8)
+  
+dtm$criteria_s <- dtm_1%>%
+  summarise(mean = round(rowMeans(., na.rm = T), 2)) # criteria score
+  
 dtm$criteria_s <- unlist(dtm$criteria_s)
 dtm$criteria_s <- as.numeric(dtm$criteria_s)
 
-dtm_long <- gather(dtm_wide, Criteria, value, 'Data collection methods':'Timeframe for access', factor_key = TRUE)
 
+    
+plot2 <- dtm %>%
+      mutate(Criteria = reorder(Criteria, criteria_s))%>%
+      ggplot(aes(Criteria, criteria_s)) +
+      geom_col(show.legend = FALSE, alpha = .5) +
+      scale_x_reordered() +
+      coord_flip() +
+      geom_text(aes(label = criteria_s, hjust = 0.4)) +
+      labs(x = NULL, y = "Average Score") +
+      theme(axis.text.x = element_text(color = "black"),
+            axis.text.y = element_text(color = "black"))
 
-dtm_wide <- spread(dtm_long, Criteria, value)
-str(dtm_wide)
-
-#----average score for each plan
-dtm_wide_1 <- dplyr::select(dtm_wide, 'Data collection methods':'Timeframe for access')
-dtm_wide$plan_s <- dtm_wide_1%>%
-  summarise(mean = round(rowMeans(., na.rm = T), 2))
-dtm_wide$plan_s_c <- ifelse(dtm_wide$plan_s>=1, "DMPs with average scores ≥ 1", "DMPs with average score <1")
-str(dtm_wide)
-dtm_wide$plan_s <- unlist(dtm_wide$plan_s)
-dtm_wide$plan_s <- as.numeric(dtm_wide$plan_s)
-
-dtm_wide$plan_s_c <- unlist(dtm_wide$plan_s_c)
-dtm_wide$plan_s_c <- as.factor(dtm_wide$plan_s_c)
-
-#---word count
-WC <- read_excel("/Users/minhpham/Downloads/DMP_Scoring.xlsx", 2)
-str(WC)
-WC <- as.data.frame(WC)
-WC$Plan <- as.factor(WC$Plan)
-
-
-dtm_wide <- dplyr::left_join(dtm_wide, WC, by = "Plan")
-
-
-
-
-#dtm_wide_1 <- dtm_wide%>%
-group_by(plan_s)%>%
-  summarize(count=n()) # count unique value
-
-#---Visualizations and Analysis
-#----Figure 1. Composite score distribution for data management plans (N = 21)
-ggplot(dtm_wide, aes(plan_s)) +
-  geom_bar() + 
-  theme_bw() +
-  geom_bar(alpha = .5) +
-  theme(strip.text=element_text(size=11),
-        panel.border = element_blank(),
-        panel.grid.major = element_blank(),
-        axis.line = element_line(colour = "black"),
-        axis.text = element_text(color="black", size = 10)
-  ) +
-  labs(x = "Score", y = "Count") 
-
-#----Composite scores of data management plans for Table 1.
-psych::describe(dtm_wide$plan_s) # total
-
-dtm_wide%>% # composite scores by sets
-  group_by(plan_s_c)%>%
-  summarize(n = n(),
-            mean = mean(plan_s),
-            sd = sd(plan_s),
-            median = median(plan_s),
-            min = min(plan_s),
-            max = max(plan_s)
-  )
-
-#----Figure 2. Scores of individual scorecard criteria
-dtm %>%
-  mutate(Criteria = reorder(Criteria, criteria_s))%>%
-  ggplot(aes(Criteria, criteria_s)) +
-  geom_col(show.legend = FALSE, fill = "grey") +
-  scale_x_reordered() +
-  coord_flip() +
-  theme_bw()+
-  theme(strip.text=element_text(size=11),
-        panel.border = element_blank(),
-        panel.grid.major = element_blank(),
-        axis.line = element_line(colour = "black"),
-        axis.text = element_text(color="black", size = 10)) +
-  labs(x = NULL, y = "Composite scores") +
-  geom_text(aes(label = paste0(round(criteria_s, 2), ""), hjust = .3))
+plot2 + theme_apa()
 
 #----Figure 3. Criteria scores: DMPs with scores averaging ≥1 and with scores averaging <1
-dtm_long$plan_s_c_n <- ifelse(dtm_long$plan_s_c== "DMPs with average score <1", "Average scores < 1",
-                              "Average scores ≥ 1")
-dtm_long%>%
-  group_by(plan_s_c_n, Criteria)%>%
+dtm_long <- gather(dtm, Criteria, value, 'O1':'A8', factor_key = TRUE)
+dtm_long <- gather(dtm, Plan, value, 'O1':'A8', factor_key = TRUE)
+
+
+plan_s <- dtm_long%>%
+  group_by(Plan)%>%
+  summarise(score = round(mean(value, na.rm = T), 2))
+plan_s$score_1 <- ifelse(plan_s$score>=1, "Sufficient DDOMPs", "Insufficient DDOMPs")
+plan_s <- plan_s [, -2]
+
+dtm_n <- dtm[, -23]
+dtm_long_n <- left_join(dtm_long, plan_s, by = "Plan")
+
+
+plot3 <- dtm_long_n%>%
+  group_by(score_1, Criteria)%>%
   summarise(mean = mean(value, na.rm = T))%>%
   mutate(Criteria = reorder(Criteria, mean))%>%
   ggplot(aes(Criteria, mean)) +
-  facet_wrap(~ plan_s_c_n) + 
+  facet_wrap(~ score_1) + 
   theme_bw() +
   geom_col(alpha = .5) +
   scale_x_reordered() +
   coord_flip() +
-  theme(strip.text=element_text(size=11),
-        panel.border = element_blank(),
-        panel.grid.major = element_blank(),
-        axis.line = element_line(colour = "black"),
-        axis.text = element_text(color="black", size = 10)) +
-  labs(x = NULL, y = "Composite scores") +
+  labs(x = NULL, y = "Average Score") +
   geom_text(aes(label = paste0(round(mean, 2), ""), hjust = .3)) 
 
+plot3 + theme_apa()
 
-#----Figure 4. Wordcount vs. composite score
-dtm_wide$Set <- dtm_wide$plan_s_c
-ggplot(dtm_wide, aes(x = Wordcount, y = plan_s, color = Set, shape = Set)) +
+#---word count
+WC <- read_excel("/Users/minhpham/Downloads/DMP_Scoring.xlsx", 2)
+WC <- as.data.frame(WC)
+WC$Plan <- as.factor(WC$Plan)
+
+plan_s <- dtm_long%>%
+  group_by(Plan)%>%
+  summarise(score = round(mean(value, na.rm = T), 2))
+plan_s$Set <- ifelse(plan_s$score>=1, "Sufficient DDOMPs", "Insufficient DDOMPs")
+
+plan_s <- dplyr::left_join(plan_s, WC, by = "Plan")
+
+plot4 <- ggplot(plan_s, aes(x = Wordcount, y = score, color = Set, shape = Set)) +
   geom_point(size = 3) +
-  theme_bw()+
-  geom_point(alpha = .5) +
-  theme(strip.text=element_text(size=11),
-        panel.border = element_blank(),
-        panel.grid.major = element_blank(),
-        axis.line = element_line(colour = "black"),
-        axis.text = element_text(color="black", size = 10),
-        legend.position = "bottom") +
-  labs(x = "Word count", y = "Composite scores") +
+  geom_point() +
+  theme(legend.position = "bottom") +
+  labs(x = "Word Count", y = "Average Score") +
   expand_limits(x = 0, y = 0) 
 
-#----Table 4. Average word counts of Sample DMPs across the two sets
-psych::describe(WC$Wordcount)
-dtm_wide%>% # Average word counts by sets
-  group_by(plan_s_c)%>%
-  summarize(n = n(),
-            mean = mean(Wordcount),
-            sd = sd(Wordcount),
-            median = median(Wordcount),
-            min = min(Wordcount),
-            max = max(Wordcount)
-  )
+
+plot4 + theme_apa()
 
 
 
-str(dtm_wide_n)
-dtm_wide_n_1 <- dtm_wide_n%>%
-  filter(plan_s_c == "DMPs with average scores ≥ 1")
-psych::describe(dtm_wide_n_1$Wordcount)
 
-dtm_wide_n_1 <- dtm_wide_n%>%
-  filter(plan_s_c == "DMPs with average score <1")
-psych::describe(dtm_wide_n_1$Wordcount)
 
-psych::describe(WC$Wordcount)
+
 
